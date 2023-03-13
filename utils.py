e379474c4c51db49f56c86613690e0856eedd306
Firstmirror94 <firstmirror94yandex.ru>
2023-03-13 13:24:26 +0300

    refactoring

--- utils.py
+++ utils.py
@@ -1,11 +1,13 @@
 from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
-from clarifai_grpc.grpc.api.status import status_pb2, status_code_pb2
+from clarifai_grpc.grpc.api.status import status_code_pb2
+
 from emoji import emojize
 from random import randint, choice
 from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 import settings
+
 
 def get_smile(user_data):
     if "emoji" not in user_data:
@@ -13,41 +15,50 @@ def get_smile(user_data):
         return emojize(smile, language="alias")
     return user_data["emoji"]
 
+
 def play_random_numbers(user_number):
     bot_number = randint(user_number - 10, user_number + 10)
     if user_number > bot_number:
-        message = f"Ваше число {user_number}, моё число {bot_number}, вы выйграли!"
+        message = f"""Ваше число {user_number}, моё число {bot_number},
+         вы выйграли!"""
     elif user_number == bot_number:
         message = f"Ваше число {user_number}, моё число {bot_number}, ничья!"
     else:
-        message = f"Ваше число {user_number}, моё число {bot_number}, вы проиграли!"
-    return message   
+        message = f"""Ваше число {user_number}, моё число {bot_number},
+         вы проиграли!"""
+    return message
+
 
 def main_keyboard():
-    return ReplyKeyboardMarkup([
-        ["Прислать котика", KeyboardButton("Мои координаты",
-            request_location=True), "Заполнить анкету", ]
-    ])
+    return ReplyKeyboardMarkup(
+        [
+            [
+                "Прислать котика",
+                KeyboardButton("Мои координаты", request_location=True),
+                "Заполнить анкету",
+            ]
+        ]
+    )
+
 
 def has_object_in_image(file_name, object_name):
     channel = ClarifaiChannel.get_grpc_channel()
     app = service_pb2_grpc.V2Stub(channel)
-    metadata = (('authorization', f'Key {settings.CLARIFAI_API_KEY}'),)
+    metadata = (("authorization", f"Key {settings.CLARIFAI_API_KEY}"),)
 
     with open(file_name, "rb") as f:
         file_data = f.read()
         image = resources_pb2.Image(base64=file_data)
 
     request = service_pb2.PostModelOutputsRequest(
-        model_id='aaa03c23b3724a16a56b629203edc62c',
-        inputs=[
-            resources_pb2.Input(data=resources_pb2.Data(image=image)
-            )
-        ])
+        model_id="aaa03c23b3724a16a56b629203edc62c",
+        inputs=[resources_pb2.Input(data=resources_pb2.Data(image=image))],
+    )
 
     response = app.PostModelOutputs(request, metadata=metadata)
     # print(response)
     return check_response_for_object(response, object_name)
+
 
 def check_response_for_object(response, object_name):
     if response.status.code == status_code_pb2.SUCCESS:
@@ -55,7 +66,8 @@ def check_response_for_object(response, object_name):
             if concept.name == object_name and concept.value >= 0.90:
                 return True
     else:
-        print(f"Ошибка распознования картинки {response.outputs[0].status.details}")
+        print(f"""Ошибка распознования картинки
+         {response.outputs[0].status.details}""")
 
     return False
 
